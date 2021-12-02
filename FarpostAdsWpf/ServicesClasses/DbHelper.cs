a19e136e76fda01ed4c31dcbc6cb167705316f01
Analdin <alexartsmm@gmail.com>
2021-12-02 12:46:47 +0300

    structure

---
+++ FarpostAdsWpf/ServicesClasses/DbHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace FarpostJob.ServicesClasses
+{
+    public class DbHelper
+    {
+        private const string Server = "37.140.192.191";
+        private const string DatabaseName = "FarpostBd";
+        private const string UserName = "u1486803_admin";
+        private const string Password = "pR5iQ4zF7ovX6l";
+
+        public readonly MySqlConnection Connection;
+
+        public DbHelper(MySqlConnection connection)
+        {
+            this.Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        public DbHelper()
+            : this(new MySqlConnection($"Server={Server}; database={DatabaseName}; UID={UserName}; password={Password};CharSet=UTF8;"))
+        {
+        }
+
+        public void OpenConnection()
+        {
+            this.Connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            this.Connection.Close();
+        }
+
+        // For Insert, Update or Delete queries
+        public void ExecuteNonQuery(string query)
+        {
+            using (var cmd = new MySqlCommand(query, this.Connection))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int ExecuteSimpleQueryAsInt(string query)
+        {
+            return int.Parse(this.ExecuteSimpleQueryAsString(query));
+        }
+
+        public string ExecuteSimpleQueryAsString(string query)
+        {
+            return this.ExecuteSimpleQuery(query).ToString();
+        }
+
+        private object ExecuteSimpleQuery(string query)
+        {
+            using (var cmd = new MySqlCommand(query, this.Connection))
+            {
+                cmd.ExecuteNonQuery();
+
+                var reader = cmd.ExecuteReader();
+
+                if (!reader.Read())
+                    throw new Exception("Incorrect command?");
+
+                return reader.GetValue(0);
+            }
+        }
+    }
+}
---
+++ FarpostAdsWpf/ServicesClasses/UsersInfo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FarpostJob.ServicesClasses;
+using MySql.Data.MySqlClient;
+using Notification.Wpf;
+
+namespace FarpostWpfJob.ServiceClasses
+{
+    /// <summary>
+    /// Информация о пользователях
+    /// </summary>
+    public class UsersInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Rules { get; set; }
+
+        /// <summary>
+        /// Проверка - успешен ли вход в программу
+        /// </summary>
+        /// <returns>Возвращает результат: успешный ли вход в программу</returns>
+        public static bool ConnectedYes()
+        {
+            var notificationManager = new NotificationManager();
+
+            bool connected = true;
+
+            if (connected == true)
+            {
+                try
+                {
+                    DbHelper dbHelper = new DbHelper();
+                    dbHelper.OpenConnection();
+
+                    notificationManager.Show(new NotificationContent
+                    {
+                        Title = "Уведомление",
+                        Message = "Подключились к базе данных",
+                        Type = NotificationType.Information
+                    });
+                }
+                catch (Exception ex)
+                {
+                    notificationManager.Show(new NotificationContent
+                    {
+                        Title = "Ошибка",
+                        Message = "Произошла ошибка при соединении с базой данных",
+                        Type = NotificationType.Error
+                    });
+                }
+            }
+            else
+            {
+                try
+                {
+                    connected = false;
+                }
+                catch (Exception ex)
+                {
+                    notificationManager.Show(new NotificationContent
+                    {
+                        Title = "Ошибка",
+                        Message = "Произошла ошибка при соединении с базой данных",
+                        Type = NotificationType.Error
+                    });
+                }
+            }
+
+            return connected;
+        }
+    }
+
+    public class UsersReport
+    {
+        /// <summary>
+        /// Отчет о входе в программу
+        /// </summary>
+        /// <param name="enterYes">Объект для вставки</param>
+        public void GenerateReport()
+        {
+
+        }
+    }
+}
